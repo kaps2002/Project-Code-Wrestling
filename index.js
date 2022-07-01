72aad2f4db95320691fb42ea3749a7a0005a54e2
Karan Pandey <karpandey2002@gmail.com>
2022-07-01 12:53:26 +0530

    Added Javascript and New pages

---
+++ index.js
@@ -0,0 +1,12 @@
+function spark (){
+    setTimeout(()=>{document.getElementById("spark").style.color="white"},10);
+    setTimeout(()=>{document.getElementById("spark").style.backgroundColor="#28a745"},10);
+    setTimeout(()=>{document.getElementById("spark").style.color="#28a745"},500);
+    setTimeout(()=>{document.getElementById("spark").style.backgroundColor="transparent"},500);
+    setTimeout(()=>{document.getElementById("spark2").style.color="#28a745"},500);
+    setTimeout(()=>{document.getElementById("spark2").style.backgroundColor="transparent"},500);
+    setTimeout(()=>{document.getElementById("spark2").style.color="white"},500);
+    setTimeout(()=>{document.getElementById("spark2").style.backgroundColor="#28a745"},500);
+    setTimeout(()=>{document.getElementById("spark2").style.color="#28a745"},1000);
+    setTimeout(()=>{document.getElementById("spark2").style.backgroundColor="transparent"},1000);
+}
