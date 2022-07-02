b92502d5a61926921968c92404584d261863ee85
Karan Pandey <karpandey2002@gmail.com>
2022-07-02 15:24:17 +0530

    Completed creator page

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function typing(){
    
 }
 var i = 0;
-var txt = 'Choose the Brand...';
+var txt = 'Choose Your Brand...';
 var speed = 100;
 function typing2(){
     if (i < txt.length) {
@@ -33,5 +33,32 @@ function typing2(){
    
 }
 function showmore(){
-    
+    var a = document.getElementById("abcd");
+    var btnText = document.getElementById("mybtn");
+    if (a.style.display === "none") {
+        // a.style.display = "none";
+        // btnText.innerHTML = "Show More"; 
+        btnText.innerHTML = "Show less"; 
+        btnText.style.margin="auto";
+        btnText.style.display="flex";
+        btnText.style.alignItems="center";
+        btnText.style.justifyContent="center";
+        btnText.style.marginTop="20px";
+        a.style.display = "flex";
+        a.style.alignItems="center";
+        a.style.justifyContent="space-evenly";
+    }
+    else {
+        a.style.display = "none";
+        btnText.innerHTML = "Show More"; 
+        // btnText.innerHTML = "Show less"; 
+        // btnText.style.margin="auto";
+        // btnText.style.display="flex";
+        // btnText.style.alignItems="center";
+        // btnText.style.justifyContent="center";
+        // btnText.style.marginTop="20px";
+        // a.style.display = "flex";
+        // a.style.alignItems="center";
+        // a.style.justifyContent="space-evenly";
+    }
 }
